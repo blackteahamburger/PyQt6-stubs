81652eb0b5695ef353f15b6c48e800997eea5663
blackteahamburger <48985560+blackteahamburger@users.noreply.github.com>
2025-07-29 15:29:58 +0000

    chore: update PyQt6 stubs and version info

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 """Version numbers."""
 
 PYQT_VERSIONS = {
-    "PyQt6": "6.9.0",
+    "PyQt6": "6.9.1",
     "PyQt6-Charts": "6.9.0",
     "PyQt6-DataVisualization": "6.9.0",
     "PyQt6-Graphs": "6.9.0",
