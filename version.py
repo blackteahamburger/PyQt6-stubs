7355e288eaaf8af1349cc50489107cd5f0b651a6
blackteahamburger <blackteahamburger@outlook.com>
2025-07-29 23:33:42 +0800

    more test

--- version.py
+++ version.py
@@ -1,11 +1,11 @@
 """Version numbers."""
 
 PYQT_VERSIONS = {
-    "PyQt6": "6.9.1",
+    "PyQt6": "6.9.0",
     "PyQt6-Charts": "6.9.0",
     "PyQt6-DataVisualization": "6.9.0",
-    "PyQt6-Graphs": "6.9.0",
+    "PyQt6-Graphs": "6.8.9",
     "PyQt6-NetworkAuth": "6.9.0",
     "PyQt6-WebEngine": "6.9.0",
-    "PyQt6-QScintilla": "2.14.1",
+    "PyQt6-QScintilla": "2.14.0",
 }
